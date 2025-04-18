d7c1af439ffac30de46af614cf0720140dc1adb5
MaisAli <king_knight93@yahoo.com>
2025-04-19 00:02:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 
-const substances = [
-    { name: "Amphetamine", category: "Drugs" },
-    { name: "Arsenic", category: "Toxins" },
-    { name: "Atropine", category: "Prescription" },
-    { name: "Benzoylecgonine", category: "Drugs" },
-    { name: "Bromazepam", category: "Prescription" },
-    { name: "BMK", category: "Precursors" },
-    { name: "Butalbital", category: "Prescription" },
-    { name: "Caffeine", category: "Prescription" },
-    { name: "Cannabidiol", category: "Drugs" },
-    { name: "Cannabinol", category: "Drugs" },
-    { name: "Carisoprodol", category: "Prescription" },
+<head><meta charset="UTF-8"></head><pre style="caret-color: rgb(255, 255, 255); color: rgb(255, 255, 255); font-style: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; widows: auto; word-spacing: 0px; -webkit-tap-highlight-color: rgba(26, 26, 26, 0.3); -webkit-text-size-adjust: auto; -webkit-text-stroke-width: 0px; text-decoration: none; overflow-wrap: break-word; white-space: pre-wrap;">const substances = [
     { name: "Chloral Hydrate", category: "Toxins" },
+    { name: "Temazepam", category: "Prescription" },
+    { name: "Zolpidem", category: "Prescription" },
+    { name: "Chloroform", category: "Toxins" },
     { name: "Chlorpromazine", category: "Prescription" },
     { name: "Clonazepam", category: "Prescription" },
     { name: "Cocaine", category: "Drugs" },
     { name: "Codeine", category: "Prescription" },
     { name: "Cyclobenzaprine", category: "Prescription" },
-    { name: "Cyanide", category: "Toxins" },
-    { name: "Chloroform", category: "Toxins" },
-    { name: "Creatinine", category: "Other" }
+    { name: "Diazepam", category: "Prescription" },
+    { name: "Bromazepam", category: "Prescription" },
+    { name: "Alprazolam", category: "Prescription" },
+    { name: "Cannabinol", category: "Drugs" },
+    { name: "Benzoylecgonine", category: "Drugs" },
+    { name: "Buprenorphine", category: "Drugs" },
+    { name: "Amphetamine", category: "Drugs" },
+    { name: "BMK", category: "Precursors" },
+    { name: "Caffeine", category: "Prescription" },
+    { name: "Atropine", category: "Prescription" },
+    { name: "Lorazepam", category: "Prescription" },
+    { name: "Fentanyl", category: "Drugs" },
+    { name: "Heroin", category: "Drugs" },
+    { name: "Midazolam", category: "Prescription" },
+    { name: "Tramadol", category: "Prescription" },
+    { name: "Methadone", category: "Drugs" },
+    { name: "Phenobarbital", category: "Prescription" },
+    { name: "Creatinine", category: "Other" },
+    { name: "Oxycodone", category: "Drugs" },
+    { name: "Flunitrazepam", category: "Prescription" },
+    { name: "Hydrocodone", category: "Drugs" },
+    { name: "alprazolam", category: "Prescription" }
 ];
+
 
 let currentLang = "en";
 
@@ -38,28 +50,32 @@ function filterCategory(category) {
     if (category === "All") {
         filteredSubstances = [...substances];
     } else {
-        filteredSubstances = substances.filter(s => s.category === category);
+        filteredSubstances = substances.filter(s =&gt; s.category === category);
     }
     renderCards(filteredSubstances);
 }
 
 function searchSubstances() {
     const query = document.getElementById("searchBox").value.toLowerCase();
-    const results = substances.filter(s => s.name.toLowerCase().includes(query));
+    const results = substances.filter(s =&gt; s.name.toLowerCase().includes(query));
     renderCards(results);
 }
 
 function renderCards(list) {
     const container = document.getElementById("cardsContainer");
     container.innerHTML = "";
-    list.forEach(sub => {
+    list.forEach(sub =&gt; {
         const card = document.createElement("div");
         card.className = `card ${sub.category}`;
         card.innerText = sub.name;
-        card.onclick = () => alert(`${sub.name} profile coming soon...`);
+        card.onclick = () =&gt; {
+            const fileName = sub.name.toLowerCase().replaceAll(" ", "_") + ".html";
+            window.location.href = `substances/${fileName}`;
+        };
         container.appendChild(card);
     });
 }
 
 // Initial render
-renderCards(filteredSubstances);
+renderCards(filteredSubstances);</pre>
+;
