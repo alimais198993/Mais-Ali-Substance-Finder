b5a4009a66dd28e3a3657e75000118394c951ca3
MaisAli <king_knight93@yahoo.com>
2025-04-19 19:10:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ const substances = [
     { name: "P2NP", category: "Precursors", color: "#f0e68c" },
     { name: "N-formylamphetamine", category: "Precursors", color: "#f0e68c" },
     { name: "Phenylacetic Acid", category: "Precursors", color: "#f0e68c" },
+    { name: "Methylamine", category: "Precursors", color: "#f0e68c" },
     { name: "Fenetylline", category: "Drugs", color: "#f1948a" }
 ];
 
