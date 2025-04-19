eee30a150178bac58b5a7800ff39e8dde7ab54f0
MaisAli <king_knight93@yahoo.com>
2025-04-19 13:49:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const substances = [
     { name: "Amphetamine", category: "Drugs", color: "#f1948a" },
     { name: "Methamphetamine", category: "Drugs", color: "#f1948a" },
     { name: "MDA", category: "Stimulants", color: "#ffcccb" },
-    { name: "MDMA", category: "Drugs", color: "#f1948a" }
+    { name: "MDMA", category: "Drugs", color: "#f1948a" },
+    { name: "MDEA", category: "Stimulants", color: "#ffcccb" }
 ];
 
 let currentLang = "en";
