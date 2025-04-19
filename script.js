4e619dc0d7005e89f8dc6c008bbb61cda8c3dec8
MaisAli <king_knight93@yahoo.com>
2025-04-19 16:02:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const substances = [
     { name: "Modafinil", category: "Stimulants", color: "#ffcccb" },
     { name: "Morphine", category: "Opioids", color: "#cce5ff" },
     { name: "Codeine", category: "Opioids", color: "#cce5ff" },
+    { name: "Heroin", category: "Opioids", color: "#cce5ff" },
     { name: "Fenetylline", category: "Drugs", color: "#f1948a" }
 ];
 
