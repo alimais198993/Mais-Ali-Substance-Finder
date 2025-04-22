547e741ffcb9b9036ddbdd6fe75d01a8a5f1ece4
MaisAli <king_knight93@yahoo.com>
2025-04-23 01:23:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const substances = [
     { name: "4-hydroxyamphetamine", category: "Stimulants", color: "#ffcccb" },
     { name: "4-mta", category: "Other", color: "#d1d1e0" },
     { name: "oxazepam", category: "Benzodiazepines", color: "#e6ccff" },
+    { name: "clonazepam", category: "Benzodiazepines", color: "#e6ccff" },
     { name: "temazepam", category: "Benzodiazepines", color: "#e6ccff" },
     { name: "aceticanhydride", category: "Precursors", color: "#f0e68c" },
     { name: "diazepam", category: "Benzodiazepines", color: "#e6ccff" },
