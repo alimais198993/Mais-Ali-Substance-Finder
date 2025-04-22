096808f614e34c59440c8ceaa310cd1954087698
MaisAli <king_knight93@yahoo.com>
2025-04-23 02:01:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const substances = [
     { name: "4-hydroxyamphetamine", category: "Stimulants", color: "#ffcccb" },
     { name: "4-mta", category: "Other", color: "#d1d1e0" },
     { name: "oxazepam", category: "Benzodiazepines", color: "#e6ccff" },
+    { name: "duloxetine", category: "Antidepressants", color: "#ffe0b3" },
     { name: "clonazepam", category: "Benzodiazepines", color: "#e6ccff" },
     { name: "sertraline", category: "Antidepressants", color: "#ffe0b3" },
     { name: "venlafaxine", category: "Antidepressants", color: "#ffe0b3" },
